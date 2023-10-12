5d5fa86cd5f8a2c99f517806eaca09e5fa6b10b3
unknown <Breke64@gmail.com>
2023-10-12 14:11:45 -0400

    added bootstrap link in html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// function getApi() {
+//   var requestUrl = ' https://rawg.io/apidocs/15235aadda03481b8e49cf5d10936ba7';
+//   fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     for (var i = 0; i < data.length; i++);
+//     console.log(data); 
+//   })
+// }
