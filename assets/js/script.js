b6ad3d24c0c6b226a2421f5ffb24bcc032b9eaef
zschreier <zjschreier@gmail.com>
2023-10-13 11:27:36 -0500

    made minor changes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // function getApi() {
-//   var requestUrl = ' https://rawg.io/apidocs/15235aadda03481b8e49cf5d10936ba7';
+//   var requestUrl = ' https://api.rawg.io/games/15235aadda03481b8e49cf5d10936ba7';
 //   fetch(requestUrl)
 //   .then(function (response) {
 //     return response.json();
