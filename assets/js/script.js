9151ba59e027d9fce231fb749e78ee4bff2d4a98
unknown <Breke64@gmail.com>
2023-10-16 10:58:43 -0400

    commiting more changes to java file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var storeApi = 'https://api.rawg.io/api/stores?key=15235aadda03481b8e49cf5d10936ba7';
 
 
@@ -20,31 +19,8 @@ console.log(storeApi);
 
 
 
-// function storesApi(requestUrl) {
-// fetch(requestUrl)
-// .then(function(response){
-//   return response.json();
-//   console.log(requestUrl);
-// })
-// // .then(function (data){
-// //   var formOptions = document.getElementById('stores');
-// //   for (var i = 0; i < requestUrl.length; i++) {
-// //     var optionBtn = document.createElement('button');
-// //     optionBtn.textContent = requestUrl[i];
-// //     requestUrl.appendChild(optionBtn);
-// //     console.log(data);
-// //   }
-// //   });
-// };
-
-// async function storeApi() {
-// const response = await fetch('https://api.rawg.io/api/stores?key=15235aadda03481b8e49cf5d10936ba7');
-// const stores = await response.json();
-// console.log(stores);
-// }
 
 
-=======
 function getApi() {
   var requestUrl = ' https://rawg.io/apidocs/15235aadda03481b8e49cf5d10936ba7';
   fetch(requestUrl)
@@ -76,4 +52,25 @@ function searchForm() {
   alert("Here Are Your Games!");
  }
 }
->>>>>>> 9a79927133b4df2cabd233b311fe6f1a08a3286d
+// function storesApi(requestUrl) {
+// fetch(requestUrl)
+// .then(function(response){
+//   return response.json();
+//   console.log(requestUrl);
+// })
+// // .then(function (data){
+// //   var formOptions = document.getElementById('stores');
+// //   for (var i = 0; i < requestUrl.length; i++) {
+// //     var optionBtn = document.createElement('button');
+// //     optionBtn.textContent = requestUrl[i];
+// //     requestUrl.appendChild(optionBtn);
+// //     console.log(data);
+// //   }
+// //   });
+// };
+
+// async function storeApi() {
+// const response = await fetch('https://api.rawg.io/api/stores?key=15235aadda03481b8e49cf5d10936ba7');
+// const stores = await response.json();
+// console.log(stores);
+// }
