64869146594f3322d04f48ec3a784fbc591f6f9d
unknown <Breke64@gmail.com>
2023-10-16 10:47:19 -0400

    testing merge with classsmates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,45 @@
-// function getApi() {
-//   var requestUrl = ' https://rawg.io/apidocs/15235aadda03481b8e49cf5d10936ba7';
-//   fetch(requestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     for (var i = 0; i < data.length; i++);
-//     console.log(data); 
-//   })
+var storeApi = 'https://api.rawg.io/api/stores?key=15235aadda03481b8e49cf5d10936ba7';
+
+
+
+
+fetch(storeApi)
+.then(function(response){
+  return response.json();
+})
+.then(function (data){
+  console.log(data);
+});
+
+
+
+
+console.log(storeApi);
+
+
+
+
+// function storesApi(requestUrl) {
+// fetch(requestUrl)
+// .then(function(response){
+//   return response.json();
+//   console.log(requestUrl);
+// })
+// // .then(function (data){
+// //   var formOptions = document.getElementById('stores');
+// //   for (var i = 0; i < requestUrl.length; i++) {
+// //     var optionBtn = document.createElement('button');
+// //     optionBtn.textContent = requestUrl[i];
+// //     requestUrl.appendChild(optionBtn);
+// //     console.log(data);
+// //   }
+// //   });
+// };
+
+// async function storeApi() {
+// const response = await fetch('https://api.rawg.io/api/stores?key=15235aadda03481b8e49cf5d10936ba7');
+// const stores = await response.json();
+// console.log(stores);
 // }
+
+
