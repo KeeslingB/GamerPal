78618d93f8cd63c82fa9a92be14f4b44dadea633
Jason Yang <jasony24@outlook.com>
2023-10-16 10:41:46 -0500

    Added Search.html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+console.log("Start.");
+var button = document.getElementById('button');
+var gameTitle = '';
+var gpKey = '15235aadda03481b8e49cf5d10936ba7';
+var myKey = '07408fb112b44434827e8440cf06fe69';
+
 function getApi() {
   var requestUrl = ' https://rawg.io/apidocs/15235aadda03481b8e49cf5d10936ba7';
   fetch(requestUrl)
@@ -9,3 +15,27 @@ function getApi() {
     console.log(data); 
   })
 }
+
+
+
+gameTitle = 'grand-theft-auto-v';
+
+function getApiLinks() {
+  var requestUrl = 'https://api.rawg.io/api/games/' + gameTitle + '/stores?key=' + myKey;
+    // 'https://api.rawg.io/api/games?genres=2&ordering=-rating&key=07408fb112b44434827e8440cf06fe69';
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        console.log(data);
+      }
+      //console.log(data.results[]);
+      // for (var i = 0; i < data.length; i++) {
+      //   console.log(data);
+      // }
+    })
+}
+
+getApiLinks();
